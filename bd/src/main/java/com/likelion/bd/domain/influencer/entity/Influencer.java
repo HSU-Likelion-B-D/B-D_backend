ddbcc64c193c95a76e213420ab53aa2cf75d9370
naeunmin <babaleo115@gmail.com>
2025-08-02 22:14:55 +0900

    feat : influencer 엔티티 작성

---
+++ bd/src/main/java/com/likelion/bd/domain/influencer/entity/Influencer.java
@@ -0,0 +1,24 @@
+package com.likelion.bd.domain.influencer.entity;
+
+import com.likelion.bd.domain.user.entity.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Influencer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id; //기본키
+
+    @OneToOne
+    @JoinColumn(name = "userId") //user 엔티티의 기본키 이름인 userId와 매핑한다.
+    private User user;
+}
