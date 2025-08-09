8c2f7e137e1d382df647f93590eb46d21a2f1752
naeunmin <babaleo115@gmail.com>
2025-08-09 13:58:58 +0900

    feat : 사업장 404 예외처리 코드 추가

--- bd/src/main/java/com/likelion/bd/global/response/code/WorkPlaceErrorReponseCode.java
+++ bd/src/main/java/com/likelion/bd/global/response/code/WorkPlaceErrorReponseCode.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum WorkPlaceErrorReponseCode implements BaseResponseCode{
+    WORKPLACE_NOT_FOUND_404("WORKPLACE_NOT_FOUND_404",404, "사업장을 찾을 수 없습니다."),
     INVALID_TIME_FORMAT_400("INVALID_TIME_FORMAT_400",400,"시간 형식이 맞지 않습니다. ex) HH:MM"),
     CATEGORY_NOT_FOUND_404("CATEGORY_NOT_FOUND_404",404,"해당 카테고리가 존재하지 않습니다."),
     MOOD_NOT_FOUND_404("MOOD_NOT_FOUND_404", 404, "해당 분위기가 존재하지 않습니다."),
