e9707d11f371a4ba0da8822a3a1792f1c4ffab07
JongJin <2171266@hansung.ac.kr>
2025-08-10 00:47:23 +0900

    refactor: CustomException을 BaseException 상속 구조로 변경

--- bd/src/main/java/com/likelion/bd/global/exception/CustomException.java
+++ bd/src/main/java/com/likelion/bd/global/exception/CustomException.java
@@ -1,17 +1,10 @@
 package com.likelion.bd.global.exception;
 
 import com.likelion.bd.global.response.code.BaseResponseCode;
-import com.likelion.bd.global.response.code.ErrorResponseCode;
 
-public class CustomException extends RuntimeException{
-    private final BaseResponseCode errorCode;
+public class CustomException extends BaseException {
 
     public CustomException(BaseResponseCode errorCode) {
-        super(errorCode.getMessage());
-        this.errorCode = errorCode;
-    }
-
-    public BaseResponseCode getErrorCode() {
-        return errorCode;
+        super(errorCode);
     }
 }
