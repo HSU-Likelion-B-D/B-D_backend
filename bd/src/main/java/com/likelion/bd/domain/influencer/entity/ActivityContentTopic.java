4ad46acc3306463791313db0320cd958b7f231e7
JongJin <2171266@hansung.ac.kr>
2025-08-10 14:56:23 +0900

    fix: 테이블명 오류 수정(contnet -> content)

--- bd/src/main/java/com/likelion/bd/domain/influencer/entity/ActivityContentTopic.java
+++ bd/src/main/java/com/likelion/bd/domain/influencer/entity/ActivityContentTopic.java
@@ -11,7 +11,7 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@Table(name = "activity_contnetTopic")
+@Table(name = "activity_contentTopic")
 public class ActivityContentTopic {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
