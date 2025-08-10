15d190cecdcceafb1d931fe0b7531adade60261f
JongJin <2171266@hansung.ac.kr>
2025-08-10 16:52:54 +0900

    refactor: 연관관계 편의 메서드를 위한 Setter 추가

--- bd/src/main/java/com/likelion/bd/domain/influencer/entity/ActivityPreferTopic.java
+++ bd/src/main/java/com/likelion/bd/domain/influencer/entity/ActivityPreferTopic.java
@@ -1,13 +1,11 @@
 package com.likelion.bd.domain.influencer.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Entity
 @Getter
+@Setter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
