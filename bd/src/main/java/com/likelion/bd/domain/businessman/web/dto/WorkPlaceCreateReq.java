712ec6de6d50618ad87a0970665c8233919d6e6d
naeunmin <babaleo115@gmail.com>
2025-08-13 17:49:11 +0900

    refactor : 간단한 코드 수정

--- bd/src/main/java/com/likelion/bd/domain/businessman/web/dto/WorkPlaceCreateReq.java
+++ bd/src/main/java/com/likelion/bd/domain/businessman/web/dto/WorkPlaceCreateReq.java
@@ -10,17 +10,17 @@ import java.util.List;
 @Setter
 @Builder
 public class WorkPlaceCreateReq {
-    Long userId; //user의 기본키 ID
-    String name; //사업장 이름
-    String address; //사업장 주소
-    String detailAddress; //사업장 상세주소
-    String openTime; //영업시작시간
-    String closeTime; //영업마감시간
-    Boolean isOnline; //온라인스토어 유무
-    Long minBudget; //홍보 최저예산
-    Long maxBudget; //홍보 최대예산
-    List<Long> categoryIds; //사업장 카테고리
-    List<Long> moodIds; //사업장 분위기
-    List<Long> promotionIds; //사업장 홍보방식
+    private Long userId; //user의 기본키 ID
+    private String name; //사업장 이름
+    private String address; //사업장 주소
+    private String detailAddress; //사업장 상세주소
+    private String openTime; //영업시작시간
+    private String closeTime; //영업마감시간
+    private Boolean isOnline; //온라인스토어 유무
+    private Long minBudget; //홍보 최저예산
+    private Long maxBudget; //홍보 최대예산
+    private List<Long> categoryIds; //사업장 카테고리
+    private List<Long> moodIds; //사업장 분위기
+    private List<Long> promotionIds; //사업장 홍보방식
 
 }
