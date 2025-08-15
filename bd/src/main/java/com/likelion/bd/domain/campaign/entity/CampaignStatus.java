e8d79be67504e5a08b6248c6fbde3249b2106bf1
naeunmin <babaleo115@gmail.com>
2025-08-16 00:02:37 +0900

    feat : 캠페인 상태 Enum클래스 작성

---
+++ bd/src/main/java/com/likelion/bd/domain/campaign/entity/CampaignStatus.java
@@ -0,0 +1,18 @@
+package com.likelion.bd.domain.campaign.entity;
+
+public enum CampaignStatus {
+    WAITING("대기중"),
+    PROPOSED("제안 받음"),
+    CONFIRMED_IN_PROGRESS("확정 및 진행중"),
+    COMPLETED("완료"),
+    CANCELED("취소"),
+    PAYMENT_PENDING("입금 대기");
+
+    //ACCEPTED("수락"), REJECTED("거절")
+
+
+    private final String koLabel;
+
+    CampaignStatus(String koLabel) { this.koLabel = koLabel; }
+    public String getKoLabel() { return koLabel; }
+}
