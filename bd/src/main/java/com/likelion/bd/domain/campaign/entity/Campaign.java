ccde4fe28a935ffa7256435c801f7264d6a9060f
naeunmin <babaleo115@gmail.com>
2025-08-15 16:37:46 +0900

    feat : 캠페인 상태 및 제안서와 매핑

--- bd/src/main/java/com/likelion/bd/domain/campaign/entity/Campaign.java
+++ bd/src/main/java/com/likelion/bd/domain/campaign/entity/Campaign.java
@@ -28,9 +28,13 @@ public class Campaign {
     @Column(name = "RECEIVER_ROLE", nullable = false)
     private String receiverRole;
 
-//    @OneToOne(fetch = FetchType.LAZY, optional = false)
-//    @JoinColumn(name = "proposalId")
-//    private Proposal proposal;
+    @Enumerated(EnumType.STRING)
+    @Column(name = "STATE", nullable = false)
+    private String state;
+
+    @OneToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "proposalId")
+    private Proposal proposal;
 
 
 }
