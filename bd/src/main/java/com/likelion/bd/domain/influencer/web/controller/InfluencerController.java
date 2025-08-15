36f7f43b926224f609c2772fef55a3bb1405f243
JongJin <2171266@hansung.ac.kr>
2025-08-15 21:23:05 +0900

    feat: 인플루언서 정보 수정 메서드 API 구현

--- bd/src/main/java/com/likelion/bd/domain/influencer/web/controller/InfluencerController.java
+++ bd/src/main/java/com/likelion/bd/domain/influencer/web/controller/InfluencerController.java
@@ -3,6 +3,7 @@ package com.likelion.bd.domain.influencer.web.controller;
 import com.likelion.bd.domain.influencer.service.InfluencerService;
 import com.likelion.bd.domain.influencer.web.dto.ActivityCreateReq;
 import com.likelion.bd.domain.influencer.web.dto.ActivityCreateRes;
+import com.likelion.bd.domain.influencer.web.dto.ActivityUpdateReq;
 import com.likelion.bd.domain.influencer.web.dto.InfluencerMyPageRes;
 import com.likelion.bd.global.jwt.UserPrincipal;
 import com.likelion.bd.global.response.SuccessResponse;
@@ -30,6 +31,19 @@ public class InfluencerController {
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .body(SuccessResponse.ok(activityCreateRes));
+    }
+
+    @PutMapping("/activities") // 인플루언서 활동 수정
+    public ResponseEntity<SuccessResponse<?>> updateActivity(
+            @RequestBody @Valid ActivityUpdateReq activityUpdateReq,
+            @AuthenticationPrincipal UserPrincipal userPrincipal
+    ) {
+
+        influencerService.updateActivity(activityUpdateReq, userPrincipal.getId());
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(SuccessResponse.emptyCustom("인플루언서 정보 수정에 성공하셨습니다."));
     }
 
     @GetMapping("/mypage") // 인플루언서 활동 정보 수정
