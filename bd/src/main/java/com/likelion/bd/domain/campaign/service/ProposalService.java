363272d24a8a12bed1be8ea0102d86da33c9b4f2
JongJin <2171266@hansung.ac.kr>
2025-08-17 16:19:07 +0900

    feature: 제안서 폼 조회 메서드 틀 생성

--- bd/src/main/java/com/likelion/bd/domain/campaign/service/ProposalService.java
+++ bd/src/main/java/com/likelion/bd/domain/campaign/service/ProposalService.java
@@ -1,10 +1,14 @@
 package com.likelion.bd.domain.campaign.service;
 
+import com.likelion.bd.domain.campaign.web.dto.ProposalFormRes;
 import com.likelion.bd.domain.campaign.web.dto.ProposalWriteReq;
 import com.likelion.bd.domain.campaign.web.dto.ProposalWriteRes;
 import com.likelion.bd.global.jwt.UserPrincipal;
 
 public interface ProposalService {
+
+    // 제안서 작성/수정할 때 주어지는 정보들
+    ProposalFormRes getProposalForm(UserPrincipal userPrincipal);
 
     // 제안서 작성/수정
     ProposalWriteRes writeProposal(ProposalWriteReq proposalWriteReq, UserPrincipal userPrincipal);
