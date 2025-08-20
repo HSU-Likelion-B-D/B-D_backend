c79600fc7d71bf0b062a08c2d21f6b7c9163b5a4
JongJin <2171266@hansung.ac.kr>
2025-08-20 17:13:05 +0900

    feat: 평점에 따른 자영업자 랭킹 조회 메서드 추가

--- bd/src/main/java/com/likelion/bd/domain/businessman/repository/BusinessManRepository.java
+++ bd/src/main/java/com/likelion/bd/domain/businessman/repository/BusinessManRepository.java
@@ -2,11 +2,17 @@ package com.likelion.bd.domain.businessman.repository;
 
 import com.likelion.bd.domain.businessman.entity.BusinessMan;
 import com.likelion.bd.domain.user.entity.User;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface BusinessManRepository extends JpaRepository<BusinessMan, Long> {
     Optional<BusinessMan> findByUser(User user);
     Optional<BusinessMan> findByUserUserId(Long userId);
+
+    @Query("SELECT b FROM BusinessMan b WHERE b.reviewCount > 0 ORDER BY (b.totalScore / b.reviewCount) DESC")
+    List<BusinessMan> findTopBusinessManAverageScoreDesc(Pageable pageable);
 }
