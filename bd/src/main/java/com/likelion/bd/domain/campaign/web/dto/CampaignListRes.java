4154687b9dcf0f9ccf43dc61084a2e1d80bfb7cd
JongJin <2171266@hansung.ac.kr>
2025-08-20 20:28:21 +0900

    refactor: userId -> campaignId 변경

--- bd/src/main/java/com/likelion/bd/domain/campaign/web/dto/CampaignListRes.java
+++ bd/src/main/java/com/likelion/bd/domain/campaign/web/dto/CampaignListRes.java
@@ -3,7 +3,7 @@ package com.likelion.bd.domain.campaign.web.dto;
 import java.time.LocalDate;
 
 public record CampaignListRes(
-        Long userId,
+        Long campaignId,
         String imgUrl,
         String title,
         Long offerBudget,
