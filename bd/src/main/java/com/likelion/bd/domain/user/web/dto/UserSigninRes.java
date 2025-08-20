f84077eb436af259ca1b9b1cc6dc7b6afbb65e82
JongJin <2171266@hansung.ac.kr>
2025-08-20 22:24:38 +0900

    feat: 로그인 시, 사용자 역할 반환

--- bd/src/main/java/com/likelion/bd/domain/user/web/dto/UserSigninRes.java
+++ bd/src/main/java/com/likelion/bd/domain/user/web/dto/UserSigninRes.java
@@ -1,6 +1,10 @@
 package com.likelion.bd.domain.user.web.dto;
 
+import com.likelion.bd.domain.user.entity.UserRoleType;
+
 public record UserSigninRes(
-        String token) {
+        UserRoleType userRoleType,
+        String token
+) {
 
 }
