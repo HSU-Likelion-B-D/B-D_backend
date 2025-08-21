bb8efb71de73bbe4a89951eb1af2d215fa1b5574
JongJin <2171266@hansung.ac.kr>
2025-08-21 23:01:03 +0900

    fix: 닉네임 및 프로필 사진 반환하기 위한 로그인 DTO 수정

--- bd/src/main/java/com/likelion/bd/domain/user/web/dto/UserSigninRes.java
+++ bd/src/main/java/com/likelion/bd/domain/user/web/dto/UserSigninRes.java
@@ -3,6 +3,8 @@ package com.likelion.bd.domain.user.web.dto;
 import com.likelion.bd.domain.user.entity.UserRoleType;
 
 public record UserSigninRes(
+        String nickname,
+        String imgUrl,
         UserRoleType userRoleType,
         String token
 ) {
