26459846b7daf88e8799adc4458ed6bd73e2942c
JongJin <2171266@hansung.ac.kr>
2025-08-22 14:44:24 +0900

    feat: 결제 및 정산 관리 리스트 및 결제 상태 변경 메서드 틀 생성

--- bd/src/main/java/com/likelion/bd/domain/campaign/service/PaymentService.java
+++ bd/src/main/java/com/likelion/bd/domain/campaign/service/PaymentService.java
@@ -1,8 +1,22 @@
 package com.likelion.bd.domain.campaign.service;
 
 import com.likelion.bd.domain.campaign.entity.Campaign;
+import com.likelion.bd.domain.campaign.entity.CampaignStatus;
+import com.likelion.bd.domain.campaign.web.dto.PaymentListRes;
+import com.likelion.bd.domain.campaign.web.dto.PaymentResponseReq;
+import com.likelion.bd.global.jwt.UserPrincipal;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface PaymentService {
 
     void createPayment(Campaign campaign);
+
+    Page<PaymentListRes> showPayment(
+            UserPrincipal userPrincipal,
+            CampaignStatus status,
+            Boolean all,
+            Pageable pageable);
+
+    void updatePayment(UserPrincipal userPrincipal, PaymentResponseReq paymentResponseReq);
 }
