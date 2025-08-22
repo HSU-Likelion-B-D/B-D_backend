b5cdb46ada850cfce1d4b792da4b9fe7b6dabf02
JongJin <2171266@hansung.ac.kr>
2025-08-22 14:43:52 +0900

    feat: 결제 상태 관련 Enum 생성

--- bd/src/main/java/com/likelion/bd/domain/campaign/entity/PaymentStatus.java
+++ bd/src/main/java/com/likelion/bd/domain/campaign/entity/PaymentStatus.java
@@ -11,7 +11,7 @@ public enum PaymentStatus {
     PAYMENT_COMPLETED("결제 완료"), // 자영업자 : 돈 지급 완료
     WAITING("대기중"), // 인플루언서 : 자영업자가 돈을 지불하지 "않은" 상태
     PAYMENT_DUE("결제 받기"), // 인플루언서 : 자영업자가 돈을 지불"한" 상태
-    SETTLEMENT_COMPLETED("정산 완료"); // 모두 : 돈을 주고, 받고 완료된 상태
+    COMPLETED("정산 완료"); // 모두 : 돈을 주고, 받고 완료된 상태
 
     private final String description;
 }
